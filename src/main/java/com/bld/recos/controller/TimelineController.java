53a21710bb77adeaef8a6410645b479999757779
Ben Douthwaite <BenDouthwaite@RentalCars.com>
2018-09-05 15:43:22 +0100

    Initiaise Data on Startup

--- src/main/java/com/bld/recos/controller/TimelineController.java
+++ src/main/java/com/bld/recos/controller/TimelineController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @RestController
@@ -22,11 +21,7 @@ public class TimelineController {
             @PathVariable(value = "startTime") @DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm:ss") LocalDateTime startTime,
             @PathVariable(value = "endTime") @DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm:ss") LocalDateTime endTime
             ) {
-
-        Duration duration = Duration.between(startTime, endTime);
-
-        System.out.println("WE HAVE: " + duration);
-
         return timelineService.getTimelineBounded(startTime, endTime);
     }
+
 }
