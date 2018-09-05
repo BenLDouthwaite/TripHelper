1d781a496cf14421c4d252cae1346e755aecdc38
Josh Porter <joshporter@rentalcars.com>
2018-09-05 11:29:39 +0100

    Update annotations to allow jpa repository queries

--- src/main/java/com/bld/recos/model/Experience.java
+++ src/main/java/com/bld/recos/model/Experience.java
@@ -1,9 +1,19 @@
 package com.bld.recos.model;
 
-import javax.persistence.*;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "experience")
+@Data
 public class Experience {
 
     @Id
--- src/main/java/com/bld/recos/service/TimelineService.java
+++ src/main/java/com/bld/recos/service/TimelineService.java
@@ -1,19 +1,33 @@
 package com.bld.recos.service;
 
 import com.bld.recos.dto.Timeline;
+import com.bld.recos.model.Experience;
+import com.bld.recos.model.Journey;
+import com.bld.recos.respository.ExperienceRepository;
+import com.bld.recos.respository.JourneyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TimelineService {
 
+    @Autowired
+    private ExperienceRepository experienceRepository;
+
+    @Autowired
+    private JourneyRepository journeyRepository;
+
     public Timeline getTimeline() {
         Timeline timeline = new Timeline();
 
-        timeline.setExperiences(new ArrayList<>());
-
-        timeline.setJourneys(new ArrayList<>());
+        List<Experience> experienceList = new ArrayList<>();
+        List<Journey> journeyList = new ArrayList<>();
+        
+        timeline.setExperiences(experienceRepository.findAll());
+        timeline.setJourneys(journeyRepository.findAll());
 
         return timeline;
     }
